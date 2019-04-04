ff0a43befdfd9162f1615e8f3a15a7a6c6b94db4
mayurax <mayura.ramanayake@gmail.com>
2019-04-04 17:06:55 +0530

    Wired up the basic navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StudentsComponent } from './components/students/students.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { LibraryComponent } from './components/library/library.component';
+import { FinanceComponent } from './components/finance/finance.component';
+import { CoursesComponent } from './components/courses/courses.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { 
+    path: 'dashboard', 
+    component: DashboardComponent,
+    children: [
+      { path: 'students', component: StudentsComponent },
+      { path: 'employees', component: EmployeesComponent },
+      { path: 'library', component: LibraryComponent },
+      { path: 'finance', component: FinanceComponent },
+      { path: 'courses', component: CoursesComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
